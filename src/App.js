5134e9c38e9f42a9da3af083fe26954015892e39
Sheetal Yadav <sheetalyadav81683@gmail.com>
2024-03-13 11:20:17 +0530

    new

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Service_card from './components/Service_cards';
 
 function App() {
   return (
     <div className="App">
        <Header/>
-      <Hero/>
+       <Hero/>
+      <Service_card/>
     </div>
   );
 }
